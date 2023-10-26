b17d4bfc8408921be31d5bcf6d54c83bde026049
Anon <test.batch1@gmail.com>
2023-10-26 10:56:19 +0700

    added logicaloperator topics

--- logicaloperator.cpp
+++ logicaloperator.cpp
@@ -6,8 +6,15 @@ int main() {
     // ! = kebalikan dari logika operand nya
 
     int temp;
-
     
+    std::cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    if(temp > 0 && temp < 30){
+        std::cout << "The temperature is good!";
+    }else {
+        std::cout << "The temperature is bad!";
+    }
 
     return 0;
 }
