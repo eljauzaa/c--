452af5ce6517212a0d8af3f60f261211afb717d4
Nded <deddy.al.jauzaa@gmail.com>
2023-10-26 20:34:38 +0700

    adding lesson logical operator

--- logicaloperator.cpp
+++ logicaloperator.cpp
@@ -5,15 +5,23 @@ int main() {
     // || = cek apakah salah satu kondisi benar apa ga
     // ! = kebalikan dari logika operand nya
 
-    int temp;
+    double temp;
+    bool sunny = false;
     
     std::cout << "Enter the temperature: ";
     std::cin >> temp;
 
-    if(temp > 0 && temp < 30){
-        std::cout << "The temperature is good!";
+    // if(temp > 0 && temp < 30){
+    if(temp <= 0 || temp >= 30){
+        std::cout << "The temperature is bad!\n";
     }else {
-        std::cout << "The temperature is bad!";
+        std::cout << "The temperature is good!\n";
+    }
+
+    if(!sunny) {
+        std::cout << "It's cloudy outside!";
+    } else {
+        std::cout << "It's sunny outside!";
     }
 
     return 0;
