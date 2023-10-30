43b2dd4487567aae34c7d8e6d5a3d8c9f6f5b8bc
Nded <deddy.al.jauzaa@gmail.com>
2023-10-30 16:43:07 +0700

    adding function lesson

--- function.cpp
+++ function.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
-void happyBirthday(){
-    std::cout << "Happy Birthday to you!\n";
-    std::cout << "Happy Birthday to you!\n";
-    std::cout << "Happy Birthday dear you!\n";
-    std::cout << "Happy Birthday to you!\n";
-}
+//contoh deklarasi func sebelum main func tapi isi func nya ada setelah main func
+void happyBirthday(std::string name, int age);
 
 int main() {
     // function adalah sebuah blok kode yang dapat digunakan kembali
 
+    std::string name = "Bro";
+    int age = 21;
+    happyBirthday(name, age);
 
     return 0;
+}
+// function diletakkan setelah main func tidak akan berjalan kecuali deklarasi func sblm main func
+void happyBirthday(std::string name, int age){
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "Happy Birthday dear " << name << '\n';
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "You are " << age << " years old\n";
 }
