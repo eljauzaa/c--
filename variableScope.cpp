653cb259dd917aa3219d6c619910ca3432439178
Anon <test.batch1@gmail.com>
2023-11-03 06:35:20 +0700

    added variable scopes lesson

--- variableScope.cpp
+++ variableScope.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 
+//ini global variabel, hindari penggunaan global variabel
+int myNum = 3;
+
 void printNum();
 
 int main(){
     // variabel lokal = dideklarasikan didalam function atau blok {}
     // global variabel = dideklarasikan di luar dan berlaku untuk semua function
-    int myNum = 1;
-
+    // int myNum = 1;
+    int myNum = 5;
     printNum();
+    // :: adalah scope resolution operator tuk mengakses global variabel
+    std::cout << ::myNum << '\n';
 
     return 0;
 }
 
 void printNum(){
-    int myNum = 2;
-    std::cout << myNum;
+    // int myNum = 2;
+    std::cout << myNum << "\n";
 }
