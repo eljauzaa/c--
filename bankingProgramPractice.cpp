54da515ef20fcf299b90811193f30170dd986437
Nded <deddy.al.jauzaa@gmail.com>
2023-11-08 16:52:04 +0700

    added code in banking program

--- bankingProgramPractice.cpp
+++ bankingProgramPractice.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
+// for precission include this header file
+#include <iomanip>
 
 void showBalance(double balance);
 double deposit();
 double withdraw(double balance);
 
-int main(){
+int main()
+{
     double balance = 0;
     int choice = 0;
 
-    std::cout << "********************\n";
-    std::cout << "Enter your choice : \n";
-    std::cout << "********************\n";
-    std::cout << "1. Show Balance \n";
-    std::cout << "2. Deposit Money \n";
-    std::cout << "3. Withdraw Money \n";
-    std::cout << "4. Exit \n";
-    std::cin >> choice;
-
-    switch (choice)
+    do
     {
-    case 1:
-        showBalance(balance);
-        break;
-    case 2:
-        balance = balance + deposit();
-        break;
-    case 3:
-        balance = balance - withdraw(balance);
-        break;
-    case 1:
-        showBalance();
-        break;
-    
-    default:
-        break;
-    }
+        std::cout << "********************\n";
+        std::cout << "Enter your choice : \n";
+        std::cout << "********************\n";
+        std::cout << "1. Show Balance \n";
+        std::cout << "2. Deposit Money \n";
+        std::cout << "3. Withdraw Money \n";
+        std::cout << "4. Exit \n";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            showBalance(balance);
+            break;
+        case 2:
+            balance = balance + deposit(); // balance += deposit()
+            showBalance(balance);
+            break;
+        case 3:
+            balance = balance - withdraw(balance); // balance -= withdraw(balance)
+            showBalance(balance);
+            break;
+        case 4:
+            std::cout << "Thanks for visiting!\n";
+            break;
+
+        default:
+            std::cout << "Invalid choice\n";
+            break;
+        }
+    } while (choice != 4);
+
     return 0;
 }
 
-void showBalance(double balance){
+void showBalance(double balance)
+{
+    std::cout << "Your balance is: $ " << std::setprecision(2) << std::fixed << balance << "\n";
+}
 
+double deposit(){
+    double amount = 0;
+
+    std::cout << "Enter amount to be deposited: ";
+    std::cin >> amount;
+    return amount;
+}
+double withdraw(double balance){
+    return 0;
 }
